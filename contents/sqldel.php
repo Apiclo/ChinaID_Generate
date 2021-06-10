410810a95b33f3a3ae089d30138bb349044ecbaa
Apiclo <Apiclo@163.com>
2021-06-10 16:33:22 +0800

    删除操作

---
+++ contents/sqldel.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<?php
+$id=$_GET['id'];
+include 'sqlcon.php';
+if($_GET['act']=='del'){
+	$sqlstr='delete from ids where id='.$id.';';
+	$result=mysqli_query($conn,$sqlstr);
+	if($result){
+		echo "<script>alert('删除成功');location='/package-manager.php';</script>";
+	}else{
+		echo "<script>alert('删除失败');location='/package-manager.php';</script>";
+	}
+}
+?>
