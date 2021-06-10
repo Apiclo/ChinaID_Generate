1af05ca77ce8f36529542b2f92efaea1559920fe
Apiclo <Apiclo@163.com>
2021-06-10 16:32:49 +0800

    导航栏更新
    样式更新

--- contents/navbar.php
+++ contents/navbar.php
@@ -11,9 +11,12 @@ function navDist(){
     }
 }
 $navDist = navDist();
-$logo = '<a href="#"><img class="logo" width="36" height="36" src="../img/logo-black.png"></a>'."\n";
+$logo = '<a href="index.php"><img class="logo" width="36" height="36" src="../img/logo-black.png"></a>'."\n";
+$title = '<a href="index.php" class="title">身份证号码管理系统</a>';
 $navItemOne = '<a href="http://www.ip33.com/shenfenzheng.html"'.$navDist.'>合法性校验</a>'."\n";
-$navItemTwo = '<a href="https://github.com/Apiclo/ChinaID_Generate"'.$navDist.'>在Github上查看</a>'."\n";
+$navItemTwo = '<a href="/index-m.php"'.$navDist.'>简洁版</a>'."\n";
+$navItemTree = '<a href="/index.php"'.$navDist.'>桌面版</a>'."\n";
+$navItemFour = '<a href="https://github.com/Apiclo/ChinaID_Generate"'.$navDist.'>在Github上查看</a>'."\n";
 $floatR = '<div class="item-content">';
 ?>
 
@@ -22,9 +25,15 @@ $floatR = '<div class="item-content">';
         <div class="navbar-content">
             <?php
                 echo $logo;
+                if(uaDist()=='mobile'){}
+                else{echo $title;}
                 echo $floatR;
                 echo $navItemOne;
+                if(uaDist()=='mobile'){}
+                else{
                 echo $navItemTwo;
+                echo $navItemTree;}
+                echo $navItemFour;
                 echo '</div>'
             ?>
         </div>
