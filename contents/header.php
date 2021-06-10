ef047db0d392e2bfc2b7b49519df7ce9517af87a
Apiclo <Apiclo@163.com>
2021-06-10 16:31:59 +0800

    更新css链接和ua识别
    UA判断中，移动设备拓展到android|iphone|vivo|xiaomi|oppo|huawei|vivo|iphone_os|mobile|quark|al00

--- contents/header.php
+++ contents/header.php
@@ -1,10 +1,10 @@
 <?php //识别移动设备和桌面设备，没识别出来按桌面处理
     function uaDist(){
         $userAgent = $_SERVER['HTTP_USER_AGENT'];
-        if(preg_match("/intel|mac os|windows|desktop|ubuntu|x86|centos/i", $userAgent)) {
+        if(preg_match("/intel|windows|desktop|ubuntu|x86|centos/i", $userAgent)) {
             return 'desktop';
         }
-        elseif(preg_match("/android|iphone|mobile|quark|al00/i", $userAgent)){
+        elseif(preg_match("/android|iphone|vivo|xiaomi|oppo|huawei|vivo|iphone_os|mobile|quark|al00/i", $userAgent)){
             return 'mobile';
         }
         else{
@@ -23,6 +23,10 @@ $header = "
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no\"/>
 $cssLink/style.css\">
 $cssLink/jscss.css\">
-<script language=\"javascript\" src=\"ID_Generate/area.js\"></script>";
+$cssLink/list.css\">
+$cssLink/mid.css\">
+$cssLink/upanddown.css\">
+$cssLink/per-color.css\">
+";
 echo $header;
 ?>
